609710f1a1df7e815309365e0ab429efbcc79997
Michael McMullen <michael.mcmullen@tutelagesystems.com>
2015-06-21 16:33:02 -0400

    Reskinning
    Reskinned the Login and Register views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,48 +6,57 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>
-                Login
-            </h1>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <h1 class="page-header">
+                    Login to Billr
+                </h1>
+
+                @include('layouts.partials.messages')
+
+                <form method="POST" action="{{ URL::to('auth/login') }}">
+                    {!! csrf_field() !!}
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="remember">Remember Me</label>
+                        <input type="checkbox" id="remember" name="remember">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success btn-lg">Login to your account</button>
+                        <a href="{{ URL::to('/auth/register') }}" class="btn btn-default">Register New Account</a>
+                    </div>
+
+                </form>
+
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Forgot Password?
+                    </div>
+                    <div class="panel-body">
+                        <p>
+                            No worries, you can get a new password by clicking the button below.
+                        </p>
+                        <a href="{{ URL::to('auth/forgot') }}" class="btn btn-primary">Forgot Password</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-md-12">
-
-            <form method="POST" action="{{ URL::to('auth/login') }}">
-                {!! csrf_field() !!}
-
-                <div>
-                    Email
-                    <input type="email" name="email" value="{{ old('email') }}">
-                </div>
-
-                <div>
-                    Password
-                    <input type="password" name="password" id="password">
-                </div>
-
-                <div>
-                    <input type="checkbox" name="remember"> Remember Me
-                </div>
-
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-
-                <div>
-                    <a href="{{ URL::to('/auth/register') }}">Register Account</a>
-                </div>
-
-            </form>
-
-        </div>
-    </div>
-</div>
-
 
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,47 +6,52 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>
-                Register
-            </h1>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="page-header">
+                    Register new account
+                </h1>
+
+                <form method="POST" action="{{ URL::to('auth/register') }}">
+                    {!! csrf_field() !!}
+
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirm">Confirm Password</label>
+                        <input type="password" id="confirm" name="password_confirmation" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-success btn-lg">Register</button>
+                        <a href="{{ URL::to('/auth/login') }}" class="btn btn-default">I already have an account</a>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6">
+                <h1 class="page-header">
+                    Why Sign Up
+                </h1>
+                <p>
+                    Just because ok
+                </p>
+            </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <form method="POST" action="{{ URL::to('auth/register') }}">
-                {!! csrf_field() !!}
-
-                <div class="col-md-6">
-                    Name
-                    <input type="text" name="name" value="{{ old('name') }}">
-                </div>
-
-                <div>
-                    Email
-                    <input type="email" name="email" value="{{ old('email') }}">
-                </div>
-
-                <div>
-                    Password
-                    <input type="password" name="password">
-                </div>
-
-                <div class="col-md-6">
-                    Confirm Password
-                    <input type="password" name="password_confirmation">
-                </div>
-
-                <div>
-                    <button type="submit">Register</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
 
 @stop
